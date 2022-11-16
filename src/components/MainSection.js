3c16943ead73dd22e86b9bbd5367bfeed9cb6e91
punithashunmugam4 <punithashunmugam4@gmail.com>
2022-11-16 09:58:58 +0530

    first

---
+++ src/components/MainSection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../App.css'
+import {Button} from './Button';
+import './MainSection.css';
+
+const MainSection=()=>{
+    return(
+        <div className='main-container'>
+            <video src='/videos/video-2.mp4' autoPlay loop muted/>
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className='main-btns'>
+            <Button className='btns'
+            buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
+            <Button className='btns'
+            buttonStyle='btn--primary' buttonSize='btn--large'>WATCH TRAILER<i className='far fa-play-circle'/></Button>
+            </div>
+        </div>
+    )
+}
+
+export default MainSection
---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React,{useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css';
+
+function Navbar() {
+    const [click,setClick]=useState(false);
+    const [button,setButton]=useState(true);
+    
+    const handleClick=()=>setClick(!click);
+    const closeMenu=()=>setClick(false);
+    const showButton=()=>{
+        if(window.innerWidth<=960){
+            setButton(false);
+        }
+        else{
+            setButton(true);
+        }
+    }
+    useEffect(()=>{
+        showButton();
+    },[]);
+    window.addEventListener('resize',showButton);
+  return (
+    <>
+      <nav className='navbar'>
+      <div className='navbar-container'>
+      <Link to="/" className="navbar-logo" onClick={closeMenu}>
+        PS<i className="fa-brands fa-docker"/>
+      </Link>
+      <div className='menu-icon' onClick={handleClick}>
+        <i className={click ? 'fas fa-times':'fas fa-bars'}/>
+      </div>
+      <ul className={click ? 'nav-menu active':'nav-menu'}>
+      <li className='nav-item'>
+        <Link to='/' className='nav-links' onClick={closeMenu}>Home</Link>
+      </li>
+      <li className='nav-item'>
+        <Link to='/services' className='nav-links' onClick={closeMenu}>Services</Link>
+      </li>
+      <li className='nav-item'>
+        <Link to='/products' className='nav-links' onClick={closeMenu}>Products</Link>
+      </li>
+      <li>
+        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMenu}>Sign Up</Link>
+      </li>
+      </ul>
+      {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+      </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar
